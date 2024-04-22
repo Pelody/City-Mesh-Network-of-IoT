480a7c293a95baa9f550f360168612918dd17a30
Po-Ling Huang <86154072+Pelody@users.noreply.github.com>
2024-04-22 16:17:10 -0700

    Delete esp32_examples directory

--- esp32_examples/run-webserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-. ./venv/bin/activate
-# Compile and upload
-arduino-cli compile --fqbn esp32:esp32:esp32 webserver --build-property build.partitions=min_spiffs --build-property upload.maximum_size=1966080
-arduino-cli upload --port /dev/ttyUSB0 --fqbn esp32:esp32:esp32 webserver
