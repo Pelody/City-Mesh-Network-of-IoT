b69371969512af10df05f0fa93c8aa04eb91591d
JimmyTsien <134237030+JimmyTsien@users.noreply.github.com>
2024-04-22 16:32:08 -0700

    Update ESP32-City-collector.ino
    UART

--- ESP32_City_Mesh_IoT/ESP32-City-collector/ESP32-City-collector.ino
+++ ESP32_City_Mesh_IoT/ESP32-City-collector/ESP32-City-collector.ino
@@ -5,47 +5,62 @@
 // Maximum size of the received JSON data
 const size_t MAX_JSON_SIZE = 1024;
 
+// Function to validate JSON data
+bool isValidJson(const char *data) {
+  StaticJsonDocument<MAX_JSON_SIZE> doc;
+  DeserializationError error = deserializeJson(doc, data);
+  return !error;
+}
+
+esp_now_peer_info_t peerInfo;
+
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   // Buffer to hold the received JSON data
   char receivedJson[MAX_JSON_SIZE + 1]; // Add 1 for null terminator
 
-  // Copy the received data to the buffer
-  memcpy(receivedJson, incomingData, len);
-  receivedJson[len] = '\0'; // Null-terminate the string
+  if (len > MAX_JSON_SIZE) {
+    Serial.println("Received JSON data is too large");
+    return;
+  }
 
-  // Parse the received JSON data
+  memcpy(receivedJson, incomingData, len);
+  receivedJson[len] = '\0';
+
+  if (!isValidJson(receivedJson)) {
+    Serial.println("Received data is not valid JSON");
+    return;
+  }
+
   StaticJsonDocument<MAX_JSON_SIZE> doc;
   DeserializationError error = deserializeJson(doc, receivedJson);
   if (error) {
     Serial.println("Failed to parse received JSON");
     return;
   }
-  
-  Serial.println();
-  // Print the parsed JSON data to the Serial Monitor
-  Serial.println("Received JSON data:");
-  serializeJson(doc, Serial); // Print the JSON data in a human-readable format
-  Serial.println(); // Print a newline for better readability
-}
 
-//add peer
-esp_now_peer_info_t peerInfo;
+  Serial.println("Received JSON data:");
+  serializeJson(doc, Serial);
+  Serial.println();
+
+  // Send JSON data over UART
+  String jsonString;
+  serializeJson(doc, jsonString);
+  Serial2.write(jsonString.c_str());
+}
 
 void setup() {
   Serial.begin(115200);
+  Serial2.begin(9600);
+
   WiFi.mode(WIFI_STA);
 
-  // Initialize ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Register the receive and Send callback function
   esp_now_register_recv_cb(OnDataRecv);
-  //esp_now_register_send_cb(OnDataSent);
 
-  // Set the peer address to broadcast (all 0xFF)
   memset(&peerInfo, 0, sizeof(peerInfo));
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to add peer");
@@ -54,6 +69,5 @@ void setup() {
 }
 
 void loop() {
-
+  // Nothing to do in the loop
 }
-
